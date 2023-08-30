aaf2a479f86f483750bc69440c9f6e7fab9c47ff
Marcin Gromek <gromson101@gmail.com>
2023-08-30 10:57:32 +0200

    chore: initial config

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="container mx-auto">
+        <div className="header__top">Wiatmy na stronie haris-instal 😼</div>
+      </div>
+    </header>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors:{
+      "primary": "#e93a14",
+      "secondary": "#2c2c33",
+      "navy-100": "#00055e",
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
