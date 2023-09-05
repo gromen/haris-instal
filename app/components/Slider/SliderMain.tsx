650a55242e581a453613d8f0ee9565d4ae0aa132
Marcin Gromek <gromson101@gmail.com>
2023-09-05 19:47:20 +0200

    feature: add disabled state for arrow buttons

--- app/components/Slider/SliderMain.tsx
+++ app/components/Slider/SliderMain.tsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './sliderMain.scss';
+import './slider.scss';
 
 import styled from 'styled-components';
 
--- app/components/Slider/SliderTrustedPartner.tsx
+++ app/components/Slider/SliderTrustedPartner.tsx
@@ -4,7 +4,7 @@ import Slider from 'react-slick';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './sliderMain.scss';
+import './slider.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -12,12 +12,37 @@ import {
   faChevronRight,
 } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
+import { useState } from 'react';
 
 type ArrowProps = {
   onClick?: () => void;
 };
 
+const slides = [
+  {
+    id: 'slide-1',
+    url: '/images/logo-trusted-partner-1.svg',
+    alt: 'logo trusted partner',
+  },
+  {
+    id: 'slide-2',
+    url: '/images/logo-trusted-partner-1.svg',
+    alt: 'logo trusted partner',
+  },
+  {
+    id: 'slide-3',
+    url: '/images/logo-trusted-partner-1.svg',
+    alt: 'logo trusted partner',
+  },
+  {
+    id: 'slide-4',
+    url: '/images/logo-trusted-partner-1.svg',
+    alt: 'logo trusted partner',
+  },
+];
+
 export default function SliderTrustedPartner() {
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
   const settings = {
     dots: false,
     infinite: false,
@@ -26,14 +51,24 @@ export default function SliderTrustedPartner() {
     slidesToScroll: 1,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
+    afterChange: (currentSlide: number) => setCurrentSlide(currentSlide),
+  };
+
+  const isDisabled = (arrow = 'next'): boolean => {
+    if (arrow === 'prev') {
+      return currentSlide === 0;
+    }
+
+    return currentSlide === slides.length - 1;
   };
 
   function NextArrow({ onClick }: ArrowProps) {
     return (
       <button
-        className="left-40 absolute top-32 border-primary border-2 p-4 hover:border-navy-800 "
+        className="left-40 absolute top-32 border-primary border-2 p-4 hover:border-navy-800"
         type="button"
         onClick={onClick}
+        disabled={isDisabled()}
       >
         <FontAwesomeIcon
           icon={faChevronRight}
@@ -50,6 +85,7 @@ export default function SliderTrustedPartner() {
         className="left-40 absolute z-10 top-52 border-primary border-2 p-4 hover:border-navy-800 "
         type="button"
         onClick={onClick}
+        disabled={isDisabled('prev')}
       >
         <FontAwesomeIcon
           icon={faChevronLeft}
@@ -62,30 +98,15 @@ export default function SliderTrustedPartner() {
 
   return (
     <Slider {...settings} className="slider-main">
-      <Image
-        src="/images/logo-trusted-partner-1.svg"
-        alt="logo trusted partner"
-        width="150"
-        height="50"
-      />
-      <Image
-        src="/images/logo-trusted-partner-1.svg"
-        alt="logo trusted partner"
-        width="150"
-        height="50"
-      />
-      <Image
-        src="/images/logo-trusted-partner-1.svg"
-        alt="logo trusted partner"
-        width="150"
-        height="50"
-      />
-      <Image
-        src="/images/logo-trusted-partner-1.svg"
-        alt="logo trusted partner"
-        width="150"
-        height="50"
-      />
+      {slides.map((slide) => (
+        <Image
+          key={slide.id}
+          src={slide.url}
+          alt={slide.alt}
+          width="150"
+          height="50"
+        />
+      ))}
     </Slider>
   );
 }
