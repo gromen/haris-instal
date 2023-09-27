4c104a57b26d8c215b34e4cdf145cccb1b1c67d2
Marcin Gromek <gromson101@gmail.com>
2023-09-27 15:18:28 +0200

    feature: fix address on contact page

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -21,7 +21,7 @@ export default function ContactUs() {
             className="text-4xl text-primary"
           />
           <p className="py-3 text-sm text-gray-700">Nasze biuro</p>
-          <p className="font-bold">ul. Szkolna, Ostrów Mazowiecka</p>
+          <p className="font-bold">ul. Szkolna 17, Ostrów Mazowiecka</p>
         </div>
         <div className="grow bg-primary px-5 py-10 text-center font-semibold text-white">
           <FontAwesomeIcon
@@ -30,7 +30,7 @@ export default function ContactUs() {
           />
           <p className="py-3 text-sm">Masz pytanie?</p>
           <p className="font-bold">
-            <a href="tel:+48666666666">+48-666-666-666</a>
+            <a href="tel:+48791334999">+48-791-334-999</a>
           </p>
         </div>
         <div className="grow bg-gray-500 px-5 py-10 text-center font-semibold">
@@ -40,13 +40,13 @@ export default function ContactUs() {
           />
           <p className="py-3 text-sm text-gray-700">Napisz do nas!</p>
           <p className="font-bold">
-            <a href="mailto:kontakt@haris-instal.pl">kontakt@haris-instal.pl</a>
+            <a href="mailto:kontakt@haris-instal.pl">haris.instal@onet.pl</a>
           </p>
         </div>
       </div>
       <iframe
         className="mx-auto my-10 max-w-full grayscale lg:my-20"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2412.059573611699!2d21.906539777830897!3d52.80326407213673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471fab149d253567%3A0xba23ef1f5a64a72c!2sSzkolna%2C%2007-300%20Ostr%C3%B3w%20Mazowiecka!5e0!3m2!1spl!2spl!4v1695202430673!5m2!1spl!2spl"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d882.5710376251518!2d21.906551031599587!3d52.803553132328354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471fab149b955f8f%3A0x624f5eab1545eea4!2sSzkolna%2017%2C%2007-300%20Ostr%C3%B3w%20Mazowiecka!5e0!3m2!1spl!2spl!4v1695820526059!5m2!1spl!2spl"
         width="100%"
         height="600"
         style={{ border: 0 }}
