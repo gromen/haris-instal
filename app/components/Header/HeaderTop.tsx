158dd3f0d36420833e8b8365f43158b4c4275896
Marcin Gromek <gromson101@gmail.com>
2023-10-04 10:00:15 +0200

    feature: remove FB link

--- app/components/Header/HeaderTop.tsx
+++ app/components/Header/HeaderTop.tsx
@@ -8,7 +8,7 @@ export default function HeaderTop() {
       <div className="relative bg-secondary p-6 after:absolute after:left-0 after:top-0 after:block after:h-full after:w-full after:origin-bottom-right after:-skew-x-30 after:skew-y-0 after:bg-secondary md:basis-3/12"></div>
 
       <div className="header__socialMedia flex grow items-center justify-end bg-primary p-2 md:p-6">
-        <Link href="//www.facebook.com/haris.instal.92" target="_blank">
+        <Link href="//www.facebook.com/" target="_blank">
           <FontAwesomeIcon icon={faFacebook} className="text-4xl text-white" />
         </Link>
       </div>
