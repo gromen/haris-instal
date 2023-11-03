4af764edb73fa672625e35a957b5663abac45e07
Marcin Gromek <gromson101@gmail.com>
2023-11-03 11:00:07 +0100

    build: try to fix error - NextRouter was not mounted.

---
+++ .next/static/chunks/app/global-error-c1a98b1ef8710f4b.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[470],{5211:function(e,r,n){Promise.resolve().then(n.bind(n,9264))},9264:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return u}});var t=n(6705),o=n(955),s=n(4033);function u(e){let{error:r,reset:n}=e,u=(0,s.useRouter)();return(0,o.useEffect)(()=>{console.error(r)},[r]),(0,t.jsx)("html",{children:(0,t.jsxs)("body",{children:[(0,t.jsx)("h2",{children:"Something went wrong!"}),(0,t.jsx)("button",{onClick:()=>n(),children:"Try again"}),(0,t.jsx)("button",{onClick:()=>u.push("/"),children:"Back to home"})]})})}},9991:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(955),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,c=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function f(e,r,n){var t,s={},f=null,l=null;for(t in void 0!==n&&(f=""+n),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(l=r.ref),r)u.call(r,t)&&!i.hasOwnProperty(t)&&(s[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===s[t]&&(s[t]=r[t]);return{$$typeof:o,type:e,key:f,ref:l,props:s,_owner:c.current}}r.Fragment=s,r.jsx=f,r.jsxs=f},6705:function(e,r,n){"use strict";e.exports=n(9991)},4033:function(e,r,n){e.exports=n(290)}},function(e){e.O(0,[121,26,744],function(){return e(e.s=5211)}),_N_E=e.O()}]);
